27a3293a1d46411dbdcd2c2d8a9933bafe1abaa7
hunggun <panshuai2019ps@163.com>
2022-07-22 09:09:39 +0800

    add train and evaluate mode

--- config.py
+++ config.py
@@ -1,13 +1,24 @@
 import yaml
 
 config_path = 'yamls/multi30k.yaml'
+is_train = False
 
 with open(config_path, "r", encoding='utf-8') as f:
     cfg = yaml.load(f, yaml.SafeLoader)
 
 class Config:
-    def __init__(self):
+    def __init__(self, is_train=True):
         for key, value in cfg.items():
+            if key == 'train':
+                if is_train:
+                    for key_train, value_train in value.items():
+                        setattr(self, key_train, value_train)
+                continue
+            if key == 'evaluate':
+                if not is_train:
+                    for key_eval, value_eval in value.items():
+                        setattr(self, key_eval, value_eval)
+                continue
             setattr(self, key, value)
 
-config = Config()
+config = Config(is_train=is_train)
